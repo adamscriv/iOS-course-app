ea09e3652b9f9c4bf143686da23b02f8b3304f52
AdamScrivener <adamscrivener@adamscriveners-macbook-pro.local>
2016-01-23 17:26:31 +0000

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by AdamScrivener on 23/01/2016.
+//  Copyright © 2016 AdamScrivener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
